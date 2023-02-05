663dc28af2bf18c84f75c0a8adda08715884f44f
Raju <rajupaudelg@gmail.com>
2023-02-05 15:15:35 +0200

    api for station data

--- app.py
+++ app.py
@@ -13,4 +13,16 @@ def journey_list():
     # pandas function can generate html table
     return data.to_html(header="true", table_id="table")
 
+
+@app.route('/station')
+def station_list():
+    station_data = backend.load_meta_data()
+
+    # in case if there are multiple stations id found, then displaying only one is enough as this is same information
+    station_data = station_data.groupby('ID').first()
+
+    # pandas function can generate html table
+    return station_data.to_html(header="true", table_id="table")
+
+
 app.run()
